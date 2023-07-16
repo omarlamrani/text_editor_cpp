4b032882570e9d4515f9e05a72eff0a67985f4b6
omarlamrani <lamraniomarcs@gmail.com>
2023-07-16 18:26:32 +0100

    convert terminal to raw mode

--- main.cpp
+++ main.cpp
@@ -3,11 +3,26 @@
 #include <termios.h>
 using namespace std;
 
+struct termios og_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
+}
+
 void enableRawMode(){
+    // reads char by char instead of line+enter
+    tcgetattr(STDIN_FILENO, &og_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = og_termios;
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
 }
 
 int main() {
+    enableRawMode();
     char a;
     // reads line and stores last char to a
     while (read(STDIN_FILENO, &a, 1) == 1 && a != '0');
